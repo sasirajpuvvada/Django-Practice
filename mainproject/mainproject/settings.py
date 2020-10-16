d6ccc6aa5d8f4ffe4d5351b10c93fed696d157f6
sasi <sasirajluck@gmail.com>
2020-10-16 15:30:18 +0530

    project name changes

--- mainproject/mainproject/settings.py
+++ mainproject/mainproject/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'calculator.urls'
+ROOT_URLCONF = 'mainproject.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'calculator.wsgi.application'
+WSGI_APPLICATION = 'mainproject.wsgi.application'
 
 
 # Database
