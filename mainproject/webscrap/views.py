92a511c61f996a9b6da728641ec107312046c444
sasi <sasirajluck@gmail.com>
2020-10-18 22:36:27 +0530

    Monogo db connection added

--- mainproject/webscrap/views.py
+++ mainproject/webscrap/views.py
@@ -4,12 +4,18 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import requests
 import bs4
+from pymongo import MongoClient
 # Create your views here.
 
 def index(request):
     i = 0
     page = 1
     dict = {0:'val'}
+    dict.popitem()
+    democlient = MongoClient()
+    client = MongoClient('localhost',27017)
+    db = client['temp']
+    news = db['news']
     while True:
         url = 'https://news.ycombinator.com/news?p='
         pageStr = str(page)
@@ -28,10 +34,16 @@ def index(request):
             time = age[index].get_text()
             value = str(title+" "+link+"   "+scoreValue+"  "+time)
             dict[i] = value
+            data = {"title":title, "url":link, "points":scoreValue, "time":time}
+            news.insert_one(data)
+        
+        
         moreLink = soup.select('.morelink')
         
         if len(moreLink) == 0:
             break
         page = page + 1
+        if page == 2:
+            break
     return JsonResponse(dict)
 
