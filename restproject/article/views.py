a5112701d420a9288282b09365895c52fcc4a17c
sasi <sasirajluck@gmail.com>
2020-10-20 23:24:24 +0530

    create from end point added

--- restproject/article/views.py
+++ restproject/article/views.py
@@ -8,6 +8,16 @@ from .serializers import ArticleSerializer
 
 class ArticleView(APIView):
 
+    def post(self,request):
+
+        article = request.data.get('article')
+
+        serialize = ArticleSerializer(data = article)
+        if serialize.is_valid(raise_exception=True):
+            article_saved = serialize.save()
+        
+        return Response({"success": "Article '{}'  created successfully ".format(article_saved.title)})
+
     def get(self, request):
         articles = Article.objects.all()
         serializers = ArticleSerializer(articles, many = True)
