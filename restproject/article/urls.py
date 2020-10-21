ec0370f570fa1722b9d4c8d6d74a72d186f197bb
sasi <sasirajluck@gmail.com>
2020-10-21 20:22:30 +0530

    CRUD API articles

--- restproject/article/urls.py
+++ restproject/article/urls.py
@@ -5,4 +5,5 @@ from .views import ArticleView
 
 urlpatterns = [
     path('articles/', ArticleView.as_view()),
+    path('articles/<int:pk>', ArticleView.as_view()),
 ]
--- restproject/article/views.py
+++ restproject/article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -23,4 +23,21 @@ class ArticleView(APIView):
         serializers = ArticleSerializer(articles, many = True)
         return Response({"articles":serializers.data})
 
+    def put(self, request, pk):
+        # saved_article = Article.objects.get(pk = pk)
+        saved_article = get_object_or_404(Article,pk = pk)
+        data = request.data.get('article')
+        serializer = ArticleSerializer(instance = saved_article, data = data)
+
+        if serializer.is_valid():
+            article_saved = serializer.save()
+        
+        return Response({"success": "Article '{}' updated successfully".format(article_saved.title)})
+
+    def delete(self,request, pk):
+
+        article = get_object_or_404(Article.objects.all(), pk=pk)
+        article.delete()
+
+        return Response({"success": "Article of id '{}' deleted successfully".format(pk)})
 
